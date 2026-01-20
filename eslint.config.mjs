d58ae79598597c808426c85e3610ad7d8118e00a
Roger Dornelles <rsd.santo@outlook.com>
2026-01-19 21:52:30 -0300

    feat: dependency instalation for project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,38 @@
+import nextPlugin from "@next/eslint-plugin-next";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
 export default [
   {
-    ignores: [".next/*", "node_modules/*"],
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@next/next": nextPlugin,
+      "@typescript-eslint": tsPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      // --- REGRAS DE CÓDIGO ---
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "prefer-const": "error",
+      "no-unused-vars": "off",
+
+      // --- REGRAS DE TYPESCRIPT ---
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/consistent-type-imports": "error",
+
+      // --- REGRAS DO NEXT.JS  ---
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      "react/self-closing-comp": "error",
+    },
   },
   {
-    rules: {
-      "no-unused-vars": "warn",
-    },
+    ignores: [".next/*", "node_modules/*", "components/ui/*"],
   },
 ];
