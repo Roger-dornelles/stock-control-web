1ac5d8ef1d3b0625d9cd518162797aae8a0bdfc9
Roger Dornelles <rsd.santo@outlook.com>
2026-05-05 13:02:55 -0300

    feat: creating the aside menu for the user

--- app/admin/home/page.tsx
+++ app/admin/home/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { signOut } from "next-auth/react";
+
 const admin = () => {
   return (
     <>
       <h1>Admin</h1>
+      <button onClick={() => signOut({ callbackUrl: "/" })}>SAIR</button>
     </>
   );
 };
