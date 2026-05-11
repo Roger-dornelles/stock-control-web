996cc0a39d0291b302c68c76ff33d74daeca6c76
Roger Dornelles <rsd.santo@outlook.com>
2026-05-11 12:48:46 -0300

    feat: created struct user initial page home

--- app/user/home/page.tsx
+++ app/user/home/page.tsx
@@ -4,11 +4,14 @@ import { signOut } from "next-auth/react";
 
 const dashBoard = () => {
   return (
-    <>
-      <h1>Dashboard</h1>
+    <div className={`flex flex-col`}>
+      <div className={`w-full flex-col py-5 pl-5`}>
+        <h1 className={`text-[1.2rem] font-bold`}>Todos os Produtos</h1>
+        <p className={`text-[#666]`}>Listagem de todos os produtos.</p>
+      </div>
 
       <button onClick={() => signOut({ callbackUrl: "/" })}>SAIR</button>
-    </>
+    </div>
   );
 };
 
